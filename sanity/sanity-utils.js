4c872fe9e929e4d3b933eff0ef378b05fcd747ff
yousef <yousefelsobtasy15@gmail.com>
2025-03-09 07:10:11 +0200

    env variables

--- sanity/sanity-utils.js
+++ sanity/sanity-utils.js
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET || "production",
   apiVersion: "2024-03-05",
 });
 
