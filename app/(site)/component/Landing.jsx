c0d24523a23f87a897fe0acd532945586cd32841
yousef <yousefelsobtasy15@gmail.com>
2025-03-13 00:33:19 +0200

    finished

--- app/(site)/component/Landing.jsx
+++ app/(site)/component/Landing.jsx
@@ -1,5 +1,3 @@
-export const fetchCache = 'force-no-store';
-
 import HomeProductList from "./HomeProductList";
 import CollectionList from "./CollectionList";
 import SectionLoading from "./SectionLoading";
